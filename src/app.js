0a188e88e539d043896bc047f129323f81a556f8
Rohit <stylishrb711@gmail.com>
2022-07-17 23:23:31 +0530

    partials added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 require("./db/connect");
+const hbs = require("hbs");
+
+const partialsPath = path.join(__dirname, "../partials");
 
 app.set("view engine", "hbs");
+hbs.registerPartials(partialsPath);
 
 app.get("/", async (req, res) => {
   res.render("index");
